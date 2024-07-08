7d928fa0d19d89a1031ed99a6b0ffdf87da37a27
Dominic Wagas <dominicwagas@gmail.com>
2024-07-08 21:17:49 +0800

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: false,
+    swcMinify: true,
+    trailingSlash: true,
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
+    images: {
+        unoptimized: true,
+        domains: ["lh3.googleusercontent.com"],
+    },
+};
 
 export default nextConfig;
